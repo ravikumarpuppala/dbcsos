463781800e3f041d81530fb275bf7d097c5dd91d
Ravi Puppala <rpuppala@opentext.com>
2022-10-21 17:19:53 +0530

    adding basic layout

---
+++ src/components/footer/dbc.footer.js
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+
+const DBCFooter = () => {
+    // const [data, setData] = useState("Hello, how are you?");
+    // const [toggle, setToggle] = useState(false);
+    // const returnComment = useCallback((name) => {
+    //     //console.log(' i m in returnComment ');
+    //     return data + ":" + name;
+    // }, [data]);
+    return (
+        <div>
+            This is footer
+        </div>
+    )
+}
+
+export default DBCFooter
